906cf63268a2698cd27abb9b6f1092a09183f2b8
Tresor-Kasenda <scotttresor@gmail.com>
2023-07-15 12:24:35 +0200

    create a new design of application

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('welcome'));
 
+
 Route::get('/dashboard', fn() => view('dashboard'))->middleware([
     'auth', 'verified', UserStatusMiddleware::class
 ])->name('dashboard');
